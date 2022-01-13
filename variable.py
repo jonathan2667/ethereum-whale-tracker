45aa7f965f55d7e3b423b154b13bf5eefc8b7226
jonathan2667 <77002214+jonathan2667@users.noreply.github.com>
2022-01-13 19:59:32 +0200

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,99 @@
+serum_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x476c5E26a75bd202a9683ffD34359C0CC15be0fF.csv'
+serum = "0x476c5e26a75bd202a9683ffd34359c0cc15be0ff"
+serum_holders_file = 'HoldersUptimeData/SerumHolders.xlsx'
+
+bnb_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x4fabb145d64652a948d72533023f6e7a623c7c53.csv'
+bnb = '0x4fabb145d64652a948d72533023f6e7a623c7c53'
+bnb_holders_file = 'HoldersUptimeData/BnbHolders.xlsx'
+
+ftm_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x4e15361fd6b4bb609fa63c81a2be19d873717870.csv'
+ftm = '0x4e15361fd6b4bb609fa63c81a2be19d873717870'
+ftm_holders_file = 'HoldersUptimeData/FtmHolders.xlsx'
+
+theta_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x3883f5e181fccaf8410fa61e12b59bad963fb645.csv'
+theta = '0x3883f5e181fccaf8410fa61e12b59bad963fb645'
+theta_holders_file = 'HoldersUptimeData/ThetaHolders.xlsx'
+
+vechain_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xd850942ef8811f2a866692a623011bde52a462c1.csv'
+vechain = '0xd850942ef8811f2a866692a623011bde52a462c1'
+vechain_holders_file = 'HoldersUptimeData/VechainHolders.xlsx'
+
+sand_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x3845badAde8e6dFF049820680d1F14bD3903a5d0.csv'
+sand = '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
+sand_holders_file = 'HoldersUptimeData/SandHolders.xlsx'
+
+graph_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xc944e90c64b2c07662a292be6244bdf05cda44a7.csv'
+graph = '0xc944e90c64b2c07662a292be6244bdf05cda44a7'
+graph_holders_file = 'HoldersUptimeData/GraphHolders.xlsx'
+
+aave_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.csv'
+aave = '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'
+aave_holders_file = 'HoldersUptimeData/AaveHolders.xlsx'
+
+gala_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x15D4c048F83bd7e37d49eA4C83a07267Ec4203dA.csv'
+gala = '0x15D4c048F83bd7e37d49eA4C83a07267Ec4203dA'
+gala_holders_file = 'HoldersUptimeData/GalaHolders.xlsx'
+
+quant_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x4a220e6096b25eadb88358cb44068a3248254675.csv'
+quant = '0x4a220e6096b25eadb88358cb44068a3248254675'
+quant_holders_file = "HoldersUptimeData/QuantHolders.xlsx"
+
+maker_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2.csv'
+maker = '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2'
+maker_holders_file = 'HoldersUptimeData/MakerHolders.xlsx'
+
+curve_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xD533a949740bb3306d119CC777fa900bA034cd52.csv'
+curve = '0xD533a949740bb3306d119CC777fa900bA034cd52'
+curve_holders_file = 'HoldersUptimeData/CurveHolders.xlsx'
+
+inch_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x111111111117dc0aa78b770fa6a738034120c302.csv'
+inch = '0x111111111117dc0aa78b770fa6a738034120c302'
+inch_holders_file = 'HoldersUptimeData/InchHolders.xlsx'
+
+telcoin_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x467Bccd9d29f223BcE8043b84E8C8B282827790F.csv'
+telcoin = '0x467Bccd9d29f223BcE8043b84E8C8B282827790F'
+telcoin_holders_file = 'HoldersUptimeData/TelcoinHolders.xlsx'
+
+polymath_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x9992ec3cf6a55b00978cddf2b27bc6882d88d1ec.csv'
+polymath = '0x9992ec3cf6a55b00978cddf2b27bc6882d88d1ec'
+polymath_holders_file = 'HoldersUptimeData/PolymathHolders.xlsx'
+
+kp3r_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44.csv'
+kp3r = '0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44'
+kp3r_holders_file = 'HoldersUptimeData/Kp3rHolders.xlsx'
+
+dent_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x3597bfd533a99c9aa083587b074434e61eb0a258.csv'
+dent = '0x3597bfd533a99c9aa083587b074434e61eb0a258'
+dent_holders_file = 'HoldersUptimeData/DentHolders.xlsx'
+
+ocean_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x967da4048cD07aB37855c090aAF366e4ce1b9F48.csv'
+ocean = '0x967da4048cD07aB37855c090aAF366e4ce1b9F48'
+ocean_holders_file = 'HoldersUptimeData/OceanHolders.xlsx'
+
+kyber_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xdd974d5c2e2928dea5f71b9825b8b646686bd200.csv'
+kyber = '0xdd974d5c2e2928dea5f71b9825b8b646686bd200'
+kyber_holders_file = 'HoldersUptimeData/KyberHolders.xlsx'
+
+metal_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xF433089366899D83a9f26A773D59ec7eCF30355e.csv'
+metal = '0xF433089366899D83a9f26A773D59ec7eCF30355e'
+metal_holders_file = 'HoldersUptimeData/MetalHolders.xlsx'
+
+pnt_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x89Ab32156e46F46D02ade3FEcbe5Fc4243B9AAeD.csv'
+pnt = '0x89Ab32156e46F46D02ade3FEcbe5Fc4243B9AAeD'
+pnt_holders_file = 'HoldersUptimeData/PntHolders.xlsx'
+
+celsius_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d.csv'
+celsius = '0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d'
+celsius_holders_file = 'HoldersUptimeData/CelsiusHolders.xlsx'
+
+bread_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x558ec3152e2eb2174905cd19aea4e34a23de9ad6.csv'
+bread = '0x558ec3152e2eb2174905cd19aea4e34a23de9ad6'
+bread_holders_file = 'HoldersUptimeData/BreadHolders.xlsx'
+
+amp_file = 'HoldersEtherscan/export-tokenholders-for-contract-0xff20817765cb7f73d4bde2e66e067e58d11095c2.csv'
+amp = '0xff20817765cb7f73d4bde2e66e067e58d11095c2'
+amp_holders_file = 'HoldersUptimeData/AmpHolders.xlsx'
+
+yfi_file = 'HoldersEtherscan/export-tokenholders-for-contract-0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e.csv'
+yfi = '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e'
+yfi_holders_file = 'HoldersUptimeData/YfiHolders.xlsx'
